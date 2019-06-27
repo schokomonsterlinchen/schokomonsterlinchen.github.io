c15bb88f4a4589ec4687115a51d9dab8b575fa19
schokomonsterlinchen <victoria.karl@web.de>
2019-06-27 21:45:22 +0200

    debug stroke meter

--- TestSensors/StrokeMeter/stroke_meter.js
+++ TestSensors/StrokeMeter/stroke_meter.js
@@ -135,7 +135,7 @@ function find_maximum_point() {
     if (isNaN(averaged_array[counter_0][0])
         || isNaN(averaged_array[counter_1][0])
         || isNaN(averaged_array[counter_2][0])) {
-        stroke = "0: " + averaged_array[counter_0][0] + "; 1: " + averaged_array[counter_0][0] + "; 1: " + averaged_array[counter_0][0];
+        stroke = "isNan";
         write_stroke();
     } else if (averaged_array[counter_1][0] > averaged_array[counter_0][0]
         && averaged_array[counter_1][0] > averaged_array[counter_2][0]
@@ -149,7 +149,7 @@ function find_maximum_point() {
         stroke = "!isNaN3";
         write_stroke();
     } else {
-        stroke = "else";
+        stroke = "0: " + averaged_array[counter_0][0] + "; 1: " + averaged_array[counter_1][0] + "; 1: " + averaged_array[counter_2][0];
         write_stroke();
     }
 }

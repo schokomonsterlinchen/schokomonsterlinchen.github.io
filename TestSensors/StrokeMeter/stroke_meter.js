9f4b9cf49671f40a0a0886506372b1a3a25f715a
schokomonsterlinchen <victoria.karl@web.de>
2019-06-28 21:09:15 +0200

    debug find maximum point

--- TestSensors/StrokeMeter/stroke_meter.js
+++ TestSensors/StrokeMeter/stroke_meter.js
@@ -147,8 +147,10 @@ function find_maximum_point() {
         && averaged_array[counter_1][0] > 2
         && averaged_array[counter_2][0] > 2) {
         stroke = "!isNaN1";
+        write_stroke();
         set_maximum_point(counter_1);
         stroke = "!isNaN2";
+        write_stroke();
         set_stroke();
         stroke = "!isNaN3";
         write_stroke();
